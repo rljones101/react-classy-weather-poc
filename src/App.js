cda0e638e35087027bcd7d9874d756097bb8f08f
Rick <rjones@digitalseat.com>
2023-07-21 10:05:00 -0500

    Moved useFetchWeather hook into its own file.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useFetchWeather } from "./useFetchWeather";
 
 function getWeatherIcon(wmoCode) {
   const icons = new Map([
@@ -18,72 +19,10 @@ function getWeatherIcon(wmoCode) {
   return icons.get(arr);
 }
 
-function convertToFlag(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
-
 function formatDay(dateStr) {
   return new Intl.DateTimeFormat("en", {
     weekday: "short",
   }).format(new Date(dateStr));
-}
-
-function useFetchWeather(location) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [displayLocation, setDisplayLocation] = useState("");
-  const [weather, setWeather] = useState({});
-
-  useEffect(() => {
-    // Get weather if location changed
-    const fetchWeather = async () => {
-      if (location.length < 2) {
-        setWeather({});
-        return;
-      }
-
-      try {
-        setIsLoading(true);
-        // 1) Getting location (geocoding)
-        const geoRes = await fetch(
-          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-        );
-        const geoData = await geoRes.json();
-        console.log(geoData);
-
-        if (!geoData.results) throw new Error("Location not found");
-
-        const { latitude, longitude, timezone, name, country_code } =
-          geoData.results.at(0);
-        setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
-
-        // 2) Getting actual weather
-        const weatherRes = await fetch(
-          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-        );
-        const weatherData = await weatherRes.json();
-        setWeather(weatherData.daily);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    const prevLocation = localStorage.getItem("location");
-
-    if (location !== prevLocation) {
-      fetchWeather();
-      localStorage.setItem("location", location);
-    }
-
-    // Save it to localStorage
-  }, [location]);
-
-  return { isLoading, displayLocation, weather };
 }
 
 function App() {
